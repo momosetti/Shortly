2eeccbe4219a5cd7e53329e41bfcf159a522e540
momosetti <zinasetti@gmail.com>
2021-03-03 17:14:01 +0100

    feat: add header component

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+export default function Header() {
+  useEffect(() => {
+    const mobileNav = document.querySelector("#m_nav");
+    const nav = document.querySelector("#nav");
+    mobileNav.addEventListener("click", () => {
+      nav.classList.toggle("hidden");
+    });
+  }, []);
+
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <img src="/logo.svg" alt="Logo" />
+        </div>
+        <nav className="nav hidden" id="nav">
+          <ul>
+            <li className="nav_link">Features</li>
+            <li className="nav_link">Pricing</li>
+            <li className="nav_link">Resources</li>
+
+            <div className="members">
+              <span className="login_link">Login</span>
+              <button className="signup_btn">Sign Up</button>
+            </div>
+          </ul>
+        </nav>
+        <div className="m_menu" id="m_nav">
+          <img src="/mobile-navigation.svg" alt="Mobile Nav" />
+        </div>
+      </div>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 export default function Home() {
-  return <h1>home</h1>;
+  return <Header />;
 }
