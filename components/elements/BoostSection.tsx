c7501f1647fb2169d0fe81c63536952ff893e7a4
momosetti <zinasetti@gmail.com>
2021-03-05 23:59:20 +0100

    feat: add boost section component

---
+++ components/elements/BoostSection.tsx
@@ -0,0 +1,10 @@
+export default function BoostSection() {
+  return (
+    <section id="boost">
+      <div className="container">
+        <h4>Boost your links today</h4>
+        <button className="primary-btn get-started">get started</button>
+      </div>
+    </section>
+  );
+}
