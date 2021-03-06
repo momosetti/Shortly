2cd400b71b9bde0aa54041798380d11c480f11db
momosetti <zinasetti@gmail.com>
2021-03-06 14:52:37 +0100

    feat: create document page title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "../styles/app.scss";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+        <title>Shortly</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
