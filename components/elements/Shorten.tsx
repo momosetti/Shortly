a49a4cdc2612c7761b76cd07bf135580446ab464
momosetti <zinasetti@gmail.com>
2021-03-06 01:18:35 +0100

    feat: fetch data and pass result to ShortenLink component

--- components/elements/Shorten.tsx
+++ components/elements/Shorten.tsx
@@ -1,37 +1,35 @@
+import { useRef, useState } from "react";
+import ShortenLink from "./ShortenLink";
+
 export default function Shorten() {
+  const [shortenUrls, setShortenUrl] = useState([]);
+  const urlValue = useRef(null);
+  const API_ENDPOINT = "https://api.shrtco.de/v2/";
+  const handleRequest = async (e) => {
+    e.preventDefault();
+    const data = await fetch(
+      API_ENDPOINT + `shorten?url=${urlValue.current.value}`,
+      { method: "GET" }
+    );
+    const response = await data.json();
+    setShortenUrl(shortenUrls.concat(response.result));
+  };
   return (
     <section id="shorten-link">
       <div className="container">
-        <div className="shorten-form">
-          <input
-            placeholder="Shorten a link here..."
-            type="text"
-            className="shorten-input"
-          />
-          <button className="primary-btn shorten-btn">shorten it!</button>
-        </div>
+        <form onSubmit={handleRequest}>
+          <div className="shorten-form">
+            <input
+              ref={urlValue}
+              placeholder="Shorten a link here..."
+              type="text"
+              className="shorten-input"
+            />
+            <button className="primary-btn shorten-btn">shorten it!</button>
+          </div>
+        </form>
         <div className="shorten-result">
-          <div className="shorten-result__elm">
-            <p className="original-link">www.frontendmentro.io</p>
-            <div className="link-result">
-              <a href="#">https://rel.fc/cf5fc</a>
-              <button className="primary-btn copy-btn">copy</button>
-            </div>
-          </div>
-          <div className="shorten-result__elm">
-            <p className="original-link">www.frontendmentro.io</p>
-            <div className="link-result">
-              <a href="#">https://rel.fc/cf5fcdededeses</a>
-              <button className="primary-btn copy-btn">copy</button>
-            </div>
-          </div>
-          <div className="shorten-result__elm">
-            <p className="original-link">www.frontedesesndmentro.io</p>
-            <div className="link-result">
-              <a href="#">https://rel.fc/cf5fc</a>
-              <button className="primary-btn copy-btn">copy</button>
-            </div>
-          </div>
+          <ShortenLink links={shortenUrls} />
         </div>
       </div>
     </section>
