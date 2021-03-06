e53fa4cd45ddc966b7baff66adcf2f63f0820e3c
momosetti <zinasetti@gmail.com>
2021-03-06 02:41:50 +0100

    feat: implement copy to clipboard feature

--- components/elements/ShortenLink.tsx
+++ components/elements/ShortenLink.tsx
@@ -1,4 +1,16 @@
 export default function ShortenLink({ links }) {
+  const copyToClipboard = async (e) => {
+    /*
+    Use event delegation and data-* jsx attribute to get url data
+    for every shorten url with one event listener
+    I used borwser Clipboard API to copy data to user clipboard
+    */
+    await navigator.clipboard.writeText(e.target.dataset.url);
+    e.target.textContent = "Copied!";
+    setTimeout(() => {
+      e.target.textContent = "Copy";
+    }, 800);
+  };
   return (
     <>
       {links.map((link) => {
@@ -7,7 +19,13 @@ export default function ShortenLink({ links }) {
             <p className="original-link">{link.original_link}</p>
             <div className="link-result">
               <a href={link.full_short_link}>{link.full_short_link}</a>
-              <button className="primary-btn copy-btn">copy</button>
+              <button
+                data-url={link.full_short_link}
+                onClick={copyToClipboard}
+                className="primary-btn copy-btn"
+              >
+                copy
+              </button>
             </div>
           </div>
         );
